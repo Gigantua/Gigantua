2e7c5f232669c09e483fc315070c0dd7ecfd131f
Steven Nguyen <nguyeste008@students.garlandisd.net>
2022-03-27 21:40:57 -0500

    fix spelling error
    This is invalid since the exe is not updated just wanted to make this notice

--- Gigantua/Gigantua.cpp
+++ Gigantua/Gigantua.cpp
@@ -6,13 +6,13 @@
 #include "Movelist.hpp"
 #include "Chess_Test.hpp"
 
-class MoveReciever
+class MoveReceiver
 {
 public:
 	static inline uint64_t nodes;
 
 	static _ForceInline void Init(Board& brd, uint64_t EPInit) {
-		MoveReciever::nodes = 0;
+		MoveReceiver::nodes = 0;
 		Movelist::Init(EPInit);
 	}
 
@@ -34,7 +34,7 @@ public:
 		if constexpr (depth == 1)
 			PerfT1<status>(brd);
 		else
-			Movelist::EnumerateMoves<status, MoveReciever, depth>(brd);
+			Movelist::EnumerateMoves<status, MoveReceiver, depth>(brd);
 	}
 
 
@@ -192,7 +192,7 @@ public:
 template<class BoardStatus status>
 static void PerfT(std::string_view def, Board& brd, int depth)
 {
-	MoveReciever::Init(brd, FEN::FenEnpassant(def));
+	MoveReceiver::Init(brd, FEN::FenEnpassant(def));
 
 	//Seemap see;
 	//Movegen::InitBoard<status>(see, brd.UnpackAll());
@@ -202,25 +202,25 @@ static void PerfT(std::string_view def, Board& brd, int depth)
 	/// </summary>
 	switch (depth)
 	{
-		case 0: Movelist::InitStack<status, 0>(brd); MoveReciever::PerfT0<status>(); return;
-		case 1: Movelist::InitStack<status, 1>(brd); MoveReciever::PerfT1<status>(brd); return; //Keep this as T1
-		case 2: Movelist::InitStack<status, 2>(brd); MoveReciever::PerfT<status, 2>(brd); return;
-		case 3: Movelist::InitStack<status, 3>(brd); MoveReciever::PerfT<status, 3>(brd);  return;
-		case 4: Movelist::InitStack<status, 4>(brd); MoveReciever::PerfT<status, 4>(brd);  return;
-		case 5: Movelist::InitStack<status, 5>(brd); MoveReciever::PerfT<status, 5>(brd);  return;
-		case 6: Movelist::InitStack<status, 6>(brd); MoveReciever::PerfT<status, 6>(brd);  return;
-		case 7: Movelist::InitStack<status, 7>(brd); MoveReciever::PerfT<status, 7>(brd);  return;
-		case 8: Movelist::InitStack<status, 8>(brd); MoveReciever::PerfT<status, 8>(brd);  return;
-		case 9: Movelist::InitStack<status, 9>(brd); MoveReciever::PerfT<status, 9>(brd);  return;
-		case 10: Movelist::InitStack<status, 10>(brd); MoveReciever::PerfT<status, 10>(brd); return;
-		case 11: Movelist::InitStack<status, 11>(brd); MoveReciever::PerfT<status, 11>(brd); return;
-		case 12: Movelist::InitStack<status, 12>(brd); MoveReciever::PerfT<status, 12>(brd); return;
-		case 13: Movelist::InitStack<status, 13>(brd); MoveReciever::PerfT<status, 13>(brd); return;
-		case 14: Movelist::InitStack<status, 14>(brd); MoveReciever::PerfT<status, 14>(brd); return;
-		case 15: Movelist::InitStack<status, 15>(brd); MoveReciever::PerfT<status, 15>(brd); return;
-		case 16: Movelist::InitStack<status, 16>(brd); MoveReciever::PerfT<status, 16>(brd); return;
-		case 17: Movelist::InitStack<status, 17>(brd); MoveReciever::PerfT<status, 17>(brd); return;
-		case 18: Movelist::InitStack<status, 18>(brd); MoveReciever::PerfT<status, 18>(brd); return;
+		case 0: Movelist::InitStack<status, 0>(brd); MoveReceiver::PerfT0<status>(); return;
+		case 1: Movelist::InitStack<status, 1>(brd); MoveReceiver::PerfT1<status>(brd); return; //Keep this as T1
+		case 2: Movelist::InitStack<status, 2>(brd); MoveReceiver::PerfT<status, 2>(brd); return;
+		case 3: Movelist::InitStack<status, 3>(brd); MoveReceiver::PerfT<status, 3>(brd);  return;
+		case 4: Movelist::InitStack<status, 4>(brd); MoveReceiver::PerfT<status, 4>(brd);  return;
+		case 5: Movelist::InitStack<status, 5>(brd); MoveReceiver::PerfT<status, 5>(brd);  return;
+		case 6: Movelist::InitStack<status, 6>(brd); MoveReceiver::PerfT<status, 6>(brd);  return;
+		case 7: Movelist::InitStack<status, 7>(brd); MoveReceiver::PerfT<status, 7>(brd);  return;
+		case 8: Movelist::InitStack<status, 8>(brd); MoveReceiver::PerfT<status, 8>(brd);  return;
+		case 9: Movelist::InitStack<status, 9>(brd); MoveReceiver::PerfT<status, 9>(brd);  return;
+		case 10: Movelist::InitStack<status, 10>(brd); MoveReceiver::PerfT<status, 10>(brd); return;
+		case 11: Movelist::InitStack<status, 11>(brd); MoveReceiver::PerfT<status, 11>(brd); return;
+		case 12: Movelist::InitStack<status, 12>(brd); MoveReceiver::PerfT<status, 12>(brd); return;
+		case 13: Movelist::InitStack<status, 13>(brd); MoveReceiver::PerfT<status, 13>(brd); return;
+		case 14: Movelist::InitStack<status, 14>(brd); MoveReceiver::PerfT<status, 14>(brd); return;
+		case 15: Movelist::InitStack<status, 15>(brd); MoveReceiver::PerfT<status, 15>(brd); return;
+		case 16: Movelist::InitStack<status, 16>(brd); MoveReceiver::PerfT<status, 16>(brd); return;
+		case 17: Movelist::InitStack<status, 17>(brd); MoveReceiver::PerfT<status, 17>(brd); return;
+		case 18: Movelist::InitStack<status, 18>(brd); MoveReceiver::PerfT<status, 18>(brd); return;
 		default:
 			std::cout << "Depth not impl yet" << std::endl;
 			return;
@@ -240,7 +240,7 @@ void Chess_Test() {
 			auto perftvals = Test::GetElements(v[i], ' ');
 			uint64_t expected = static_cast<uint64_t>(std::strtol(perftvals[1].c_str(), NULL, 10));
 			_PerfT(fen, i);
-			uint64_t result = MoveReciever::nodes;
+			uint64_t result = MoveReceiver::nodes;
 			std::string status = expected == result ? "OK" : "ERROR";
 			if (expected == result)  std::cout << "   " << i << ": " << result << " " << status << "\n";
 			else  std::cout << "xxx -> " << i << ": " << result <<" vs " << expected << " " << status << "\n";
@@ -263,7 +263,7 @@ int main(int argc, char** argv)
 		uint64_t exptected = static_cast<uint64_t>(std::strtol(args[3].c_str(), NULL, 10));
 
 		_PerfT(def, static_cast<int>(depth));
-		if (exptected == MoveReciever::nodes) return 0;
+		if (exptected == MoveReceiver::nodes) return 0;
 		return 9;
 	}
 	if (args.size() == 3) {
@@ -277,7 +277,7 @@ int main(int argc, char** argv)
 			_PerfT(def, i);
 			auto end = std::chrono::steady_clock::now();
 			long long delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-			std::cout << "Perft " << i << ": " << MoveReciever::nodes << " " << delta / 1000 << "ms " << MoveReciever::nodes * 1.0 / delta << " MNodes/s\n";
+			std::cout << "Perft " << i << ": " << MoveReceiver::nodes << " " << delta / 1000 << "ms " << MoveReceiver::nodes * 1.0 / delta << " MNodes/s\n";
 		}
 
 		std::cout << "\nPress any key to exit..."; getchar();
@@ -308,9 +308,9 @@ int main(int argc, char** argv)
 		_PerfT(def, i);
 		auto end = std::chrono::steady_clock::now();
 		long long delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << "Perft Start " <<i<< ": "<< MoveReciever::nodes << " " << delta / 1000 <<"ms " << MoveReciever::nodes * 1.0 / delta << " MNodes/s\n";
+		std::cout << "Perft Start " <<i<< ": "<< MoveReceiver::nodes << " " << delta / 1000 <<"ms " << MoveReceiver::nodes * 1.0 / delta << " MNodes/s\n";
 	}
-	if (MoveReciever::nodes == 3195901860ull) std::cout << "OK\n\n";
+	if (MoveReceiver::nodes == 3195901860ull) std::cout << "OK\n\n";
 	else std::cout << "ERROR!\n\n";
 
 	for (int i = 1; i <= 6; i++)
@@ -319,9 +319,9 @@ int main(int argc, char** argv)
 		_PerfT(kiwi, i);
 		auto end = std::chrono::steady_clock::now();
 		long long delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << "Perft Kiwi " << i << ": " << MoveReciever::nodes << " " << delta / 1000 << "ms " << MoveReciever::nodes * 1.0 / delta << " MNodes/s\n";
+		std::cout << "Perft Kiwi " << i << ": " << MoveReceiver::nodes << " " << delta / 1000 << "ms " << MoveReceiver::nodes * 1.0 / delta << " MNodes/s\n";
 	}
-	if (MoveReciever::nodes == 8031647685ull) std::cout << "OK\n\n";
+	if (MoveReceiver::nodes == 8031647685ull) std::cout << "OK\n\n";
 	else std::cout << "ERROR!\n\n";
 
 	for (int i = 1; i <= 6; i++)
@@ -330,9 +330,9 @@ int main(int argc, char** argv)
 		_PerfT(midgame, i);
 		auto end = std::chrono::steady_clock::now();
 		long long delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << "Perft Midgame " << i << ": " << MoveReciever::nodes << " " << delta / 1000 << "ms " << MoveReciever::nodes * 1.0 / delta << " MNodes/s\n";
+		std::cout << "Perft Midgame " << i << ": " << MoveReceiver::nodes << " " << delta / 1000 << "ms " << MoveReceiver::nodes * 1.0 / delta << " MNodes/s\n";
 	}
-	if (MoveReciever::nodes == 6923051137ull) std::cout << "OK\n\n";
+	if (MoveReceiver::nodes == 6923051137ull) std::cout << "OK\n\n";
 	else std::cout << "ERROR!\n\n";
 
 	for (int i = 1; i <= 6; i++)
@@ -341,17 +341,17 @@ int main(int argc, char** argv)
 		_PerfT(endgame, i);
 		auto end = std::chrono::steady_clock::now();
 		long long delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-		std::cout << "Perft Endgame " << i << ": " << MoveReciever::nodes << " " << delta / 1000 << "ms " << MoveReciever::nodes * 1.0 / delta << " MNodes/s\n";
+		std::cout << "Perft Endgame " << i << ": " << MoveReceiver::nodes << " " << delta / 1000 << "ms " << MoveReceiver::nodes * 1.0 / delta << " MNodes/s\n";
 	}
-	if (MoveReciever::nodes == 849167880ull) std::cout << "OK\n\n";
+	if (MoveReceiver::nodes == 849167880ull) std::cout << "OK\n\n";
 	else std::cout << "ERROR!\n\n";
 
 	//Total nodes
-	MoveReciever::nodes = (3195901860ull + 8031647685ull + 6923051137ull + 849167880ull);
+	MoveReceiver::nodes = (3195901860ull + 8031647685ull + 6923051137ull + 849167880ull);
 	auto te = std::chrono::steady_clock::now();
 	long long total = std::chrono::duration_cast<std::chrono::microseconds>(te - ts).count();
-	std::cout << "Perft aggregate: " << MoveReciever::nodes
-		<< " " << total / 1000 << "ms " << MoveReciever::nodes * 1.0 / total << " MNodes/s\n";
+	std::cout << "Perft aggregate: " << MoveReceiver::nodes
+		<< " " << total / 1000 << "ms " << MoveReceiver::nodes * 1.0 / total << " MNodes/s\n";
 
 	std::cout << "\nPress any key to exit..."; getchar();
 }
